f5845402609e0ae2e9b16d6e315232be51d1ae46
Carol Chen <carolchen1120@yahoo.com>
2022-05-19 21:42:50 -0400

    Created week 1 and week 2 folders

---
+++ Week_2/get_base_number.py
@@ -0,0 +1,8 @@
+def get_base_number(num,base):
+    '''get_base_number(num,base) -> int
+    returns value of num as a base number in the given base'''
+    
+
+# test cases
+print(get_base_number('10011',2))  # should be 19
+print(get_base_number('3202',5))   # should be 427
