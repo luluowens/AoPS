71e6908ac1cd8ff1378fb75eeed722bd796bb5b9
Carol Chen <carolchen1120@yahoo.com>
2022-05-20 23:37:39 -0400

    finished railfence_cipher

--- Week_1/railfence_cipher.py
+++ Week_1/railfence_cipher.py
@@ -32,10 +32,10 @@ def decode_text(ciphertext,wordfilename):
     attempts to decode ciphertext using railfence cipher
     wordfilename is a file with a list of valid words'''
     count = 0
-    max = 0
-    text = open("wordlist.txt", "r")
+    max = 1
     deciphered = ""
-    for i in range() :
+    for i in range(2, 11) :
+        text = open(wordfilename, "r")
         words = []
         word = ""
         new_text = decipher_fence(ciphertext, i)
@@ -45,38 +45,43 @@ def decode_text(ciphertext,wordfilename):
                 word = ""
             elif new_text[val] != "," or new_text[val] != "." :
                 word += new_text[val]
-        for word in words :
-            if word in text :
-                count += 1
+        words.append(word)
+        for line in text :
+            for word in words :
+                new_line = line.strip()
+                if word == new_line :
+                    count += 1
         if count >= max :
             deciphered = new_text
-    text.close()
+            max = count
+        count = 0
+        text.close()
     return deciphered
 
 # test cases
 
-# # enciphering
-# print(encipher_fence("abcdefghi", 3))
-# # should print: cfibehadg
-# print(encipher_fence("This is a test.", 2))
-# # should print: hsi  etTi sats.
-# print(encipher_fence("This is a test.", 3))
-# # should print: iiae.h  ttTss s
-# print(encipher_fence("Happy birthday to you!", 4))
-# # should print: pidtopbh ya ty !Hyraou
+enciphering
+print(encipher_fence("abcdefghi", 3))
+# should print: cfibehadg
+print(encipher_fence("This is a test.", 2))
+# should print: hsi  etTi sats.
+print(encipher_fence("This is a test.", 3))
+# should print: iiae.h  ttTss s
+print(encipher_fence("Happy birthday to you!", 4))
+# should print: pidtopbh ya ty !Hyraou
 
-# # deciphering
-# print(decipher_fence("dcgbfae",4))
-# # should print: abcdefg
-# print(decipher_fence("hsi  etTi sats.",2))
-# # should print: This is a test.
-# print(decipher_fence("iiae.h  ttTss s",3))
-# # should print: This is a test.
-# print(decipher_fence("pidtopbh ya ty !Hyraou",4))
-# # should print: Happy birthday to you!
+# deciphering
+print(decipher_fence("dcgbfae",4))
+# should print: abcdefg
+print(decipher_fence("hsi  etTi sats.",2))
+# should print: This is a test.
+print(decipher_fence("iiae.h  ttTss s",3))
+# should print: This is a test.
+print(decipher_fence("pidtopbh ya ty !Hyraou",4))
+# should print: Happy birthday to you!
 
 # decoding
 print(decode_text(" cr  pvtl eibnxmo  yghu wou rezotqkofjsehad", 'wordlist.txt'))
 # should print: the quick brown fox jumps over the lazy dog
 print(decode_text("unt S.frynPs aPiosse  Aa'lgn lt noncIniha ", 'wordlist.txt'))
-# should print... we'll let you find out!
+# should print: It's fun learning Python in an AoPS class.
