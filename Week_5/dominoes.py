3f0c1c36f9e0b43f5baffff08d48685f3d27252e
Carol Chen <carolchen1120@yahoo.com>
2022-06-16 22:48:44 -0400

    fixed dominoes

--- Week_5/dominoes.py
+++ Week_5/dominoes.py
@@ -60,7 +60,7 @@ class Deck:
     def deal_domino(self) :
         '''deals out 1 domino
         '''
-        val = random.randint(28)
+        val = random.randint(len(self.deck))
         return self.deck.pop(val)
 
 
