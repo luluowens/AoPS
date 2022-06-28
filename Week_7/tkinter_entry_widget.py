1b8911d5da3c7fc30e01f6b7433dfd9ee3e99374
Carol Chen <carolchen1120@yahoo.com>
2022-06-28 14:49:26 -0400

    wrote tkinter_entry_widget

---
+++ Week_7/tkinter_entry_widget.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+class EntryDemo(Frame):
+    '''demo of Entry widget'''
+
+    def __init__(self,master):
+        '''EntryDemo(master) -> new EntryDemo frame'''
+        Frame.__init__(self,master)
+        self.grid()
+
+        # set up control variables
+        self.inputValue = IntVar()
+        self.outputValue = IntVar()
+
+        # set up widgets
+        Label(self,text='Input').grid(row=0,column=0)
+        Label(self,text='Input Value').grid(row=0,column=1)
+        Label(self,text='Result').grid(row=0,column=3)
+        Entry(self,width=5,textvariable=self.inputValue).grid(row=1,column=0)
+        Label(self,textvariable=self.inputValue).grid(row=1,column=1)
+        Button(self,text='>>>>>',command=self.set_output_value).grid(row=1,column=2)
+        Label(self,textvariable=self.outputValue).grid(row=1,column=3)
+
+    def set_output_value(self):
+        '''update the output value'''
+        self.outputValue.set(5*self.inputValue.get())
+
+root = Tk()
+root.title('Control Variable Demo')
+demo = EntryDemo(root)
+demo.mainloop()
