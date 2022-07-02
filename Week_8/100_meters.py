b0f9d16c7b4268716aec62da7c4a70cf15cf58f6
Carol Chen <carolchen1120@yahoo.com>
2022-07-01 22:52:16 -0400

    completed 100_meters

--- Week_8/100_meters.py
+++ Week_8/100_meters.py
@@ -98,8 +98,10 @@ class Decath400MFrame(Frame):
         '''Decath400MFrame.roll()
         handler method for the roll button click'''
         # roll both dice
-        self.dice[2*self.gameround].roll()
-        self.dice[2*self.gameround+1].roll()
+        self.dice[4*self.gameround].roll()
+        self.dice[4*self.gameround+1].roll()
+        self.dice[4*self.gameround+2].roll()
+        self.dice[4*self.gameround+3].roll()
         # if this was the first roll of the round, turn on the keep button
         if self.keepButton['state'] == DISABLED :
             self.keepButton['state'] = ACTIVE
@@ -117,9 +119,9 @@ class Decath400MFrame(Frame):
                       self.dice[2*self.gameround+1].get_top()
         self.scoreLabel['text'] = 'Score: '+str(self.score)
         self.gameround += 1  # go to next round
-        if self.gameround < 4:  # move buttons to next pair of dice
-            self.rollButton.grid(row=2,column=2*self.gameround,columnspan=2)
-            self.keepButton.grid(row=3,column=2*self.gameround,columnspan=2)
+        if self.gameround < 2:  # move buttons to next pair of dice
+            self.rollButton.grid(row=2,column=4*self.gameround,columnspan=4)
+            self.keepButton.grid(row=3,column=4*self.gameround,columnspan=4)
             self.rollButton['state'] = ACTIVE
             self.keepButton['state'] = DISABLED
         else:  # game over
